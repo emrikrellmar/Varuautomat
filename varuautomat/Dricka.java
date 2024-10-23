13a36fc464db34e68e1d8b83bbeaabd26de9dd3a
Emrik Rellmar <emrik.rellmar@gmail.com>
2024-10-23 11:26:40 +0200

    Update Dricka.java

--- varuautomat/Dricka.java
+++ varuautomat/Dricka.java
@@ -4,7 +4,7 @@ public class Dricka extends Varor {
     private int temperature;
     
     public Dricka(String name, double price, int quantity) {
-        super(name, price, quantity, 0.12); // 12% moms
+        super(name, price, quantity, 0.12); 
         this.temperature = 4; 
     }
     
